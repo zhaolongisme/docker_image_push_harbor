ce767cf6d629c55bd731ac9183ade22b21e5eed8
zhaolongisme <96033374+zhaolongisme@users.noreply.github.com>
2024-07-09 09:02:25 +0800

    Delete .github/workflows/daemon.sh

--- .github/workflows/daemon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo mkdir -p /etc/docker
-sudo chmod +x /etc/docker
-sudo cat >>/etc/docker/daemon.json<<DD
-{"insecure-registries":["111.202.106.163:20061"]}
-DD
-sudo systemctl daemon-reload 
